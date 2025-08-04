004c4ca7c281c77ce4dbc466a9df3d720c0b9834
patryk.ca <patryk.capanda@windroos.pl>
2025-08-04 10:32:55 +0200

    base project structure

---
+++ src/main/java/pl/patryk/shortener/UrlShortenerDemoApplication.java
@@ -0,0 +1,13 @@
+package pl.patryk.shortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortenerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShortenerDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/patryk/shortener/UrlShortenerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.patryk.shortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
