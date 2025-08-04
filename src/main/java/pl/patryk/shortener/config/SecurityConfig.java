090496aca00e22baac9d39122c8193d9754289cd
patryk.ca <patryk.capanda@windroos.pl>
2025-08-04 17:09:06 +0200

    add OpenAPI and Swagger docs, profile-based security

--- src/main/java/pl/patryk/shortener/config/SecurityConfig.java
+++ src/main/java/pl/patryk/shortener/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package pl.patryk.shortener.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,25 +13,46 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import pl.patryk.shortener.utils.AuthFilter;
 
+import java.util.Arrays;
+
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final Environment environment;
     private final AuthFilter authenticationFilter;
     private final AuthenticationProvider authenticationProvider;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity
-                .csrf(AbstractHttpConfigurer::disable) // postman
-                .headers(headers -> headers.frameOptions().disable()) // h2 console
+
+        boolean isDev = Arrays.stream(environment.getActiveProfiles()).anyMatch(profile -> profile.equals("dev"));
+
+        if (isDev) {
+            httpSecurity.csrf(AbstractHttpConfigurer::disable);
+            httpSecurity.headers(headers -> headers.frameOptions().disable());
+        }
+
+        httpSecurity
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/h2-console/**", "/api/auth/**").permitAll()
-                        .anyRequest().authenticated()
-                )
+                .authorizeHttpRequests(auth -> {
+                    if (isDev) {
+                        auth.requestMatchers(
+                                "/api/auth/**",
+                                "/h2-console/**",
+                                "/v3/api-docs/**",
+                                "/swagger-ui.html",
+                                "/swagger-ui/**"
+                        ).permitAll();
+                    } else {
+                        auth.requestMatchers("/api/auth/**").permitAll();
+                    }
+                    auth.anyRequest().authenticated();
+                })
                 .authenticationProvider(authenticationProvider)
-                .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class)
-                .build();
+                .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return httpSecurity.build();
     }
 }
